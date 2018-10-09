d79fa95b7f08e3199a54913effac410716b15fbc
Anton Bernatskiy (kohama) <abernats@uvm.edu>
2018-10-09 16:26:07 -0700

    Simple render example is now up to date
    	modified:   tests/cube.py

--- tests/cube.py
+++ tests/cube.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
 
 import icq
+import numpy as np
+
+cubeicq = './shapes/cube4.icq'
+outfile = 'cube.png'
 
 ish = icq.ICQShape()
-ish.readICQ('./shapes/cube4.icq')
-scn = ish.getScene(cameraLocation=[100,100,40], lightColor=[2,2,2], lightLocation=[50,100,20], objectColor=[0.5,0.5,1.5])
-scn.render('cube.png', height=1024, width=1024, antialiasing=0.001)
+print('Rendering {} to {}'.format(cubeicq, outfile))
+ish.readICQ(cubeicq)
+ish.renderSceneSpherical(outfile, cameraR=200., cameraTheta=3*np.pi/8, cameraPhi=np.pi/5,
+                                  lightR=100., lightTheta=np.pi/4, lightPhi=np.pi/2,
+	                                lightColor=(2,2,2), objectColor=(0.5,0.5,1.5))

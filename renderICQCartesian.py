40fb7c350d14e990609b027653aa6c13dc06693b
Anton Bernatskiy (kohama) <abernats@uvm.edu>
2019-04-02 14:35:03 -0700

    Rotation phase support added to renderICQCartesian
    	modified:   renderICQCartesian.py

--- renderICQCartesian.py
+++ renderICQCartesian.py
@@ -12,6 +12,7 @@ parser.add_argument('camX', metavar='camX', type=float)
 parser.add_argument('camY', metavar='camY', type=float)
 parser.add_argument('camZ', metavar='camZ', type=float)
 parser.add_argument('pngFileName', metavar='pngFileName', type=str, nargs='?')
+parser.add_argument('phase', metavar='phase', type=float, nargs='?')
 
 cliArgs = parser.parse_args()
 icqFileName = cliArgs.icqFileName
@@ -19,6 +20,7 @@ camX = cliArgs.camX
 camY = cliArgs.camY
 camZ = cliArgs.camZ
 pngFileName = cliArgs.icqFileName[:-4] + '_x{}_y{}_z{}.png'.format(camX, camY, camZ) if cliArgs.pngFileName is None else cliArgs.pngFileName
+phase = cliArgs.phase
 
 print('Rendering shape model {} to {}'.format(icqFileName, pngFileName))
 
@@ -33,4 +35,5 @@ ish.renderSceneCartesian(pngFileName, cameraLocation=[camX, camY, camZ],
                                       lightLocation=[camX, camY, camZ], lightColor=[brightness, brightness, brightness],
                                       backgroundColor=(0,0,0), objectColor=[0.5,0.5,0.5],
                                       width=renderWidth, height=renderHeight,
+                                      rotationAxis=(-1,1,-1), rotationAngle=phase*6.28/75.,
                                       antialiasing=0.01)
